193304c706eb15706c92dbc464279648ba4d78cc
mliyanage@gmail.com <manjulaliyanage@badger.local>
2025-12-28 22:13:38 +1100

    build issue

--- packages/worker/.eslintrc.js
+++ packages/worker/.eslintrc.js
@@ -21,5 +21,5 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
-  ignorePatterns: ['dist', 'node_modules', '*.js'],
+  ignorePatterns: ['dist', 'node_modules', '*.js', '*.d.ts'],
 };
